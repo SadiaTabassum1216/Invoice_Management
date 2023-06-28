0338e9117e855ae4b534c4eb232a7b34050d31ec
Sadia Tabassum <60074200+SadiaTabassum1216@users.noreply.github.com>
2023-06-28 15:48:57 +0600

    all

---
+++ spice/source/main/src/app/extra-pages/users/edit-modal/edit-modal.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-edit-modal',
+  templateUrl: './edit-modal.component.html',
+  styleUrls: ['./edit-modal.component.scss']
+})
+export class EditModalComponent {
+  selectedUser: User = new User(0, '', '', '');
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private http: HttpClient,  public dialog: MatDialog) {
+    console.log(data.user); 
+    this.selectedUser=data.user;
+    console.log(this.selectedUser);
+  }
+
+  updateUser() {
+    this.http.put<any>(`http://localhost:8000/api/users/${this.selectedUser.id}`, this.selectedUser).subscribe(data => {
+  console.log(this.selectedUser);
+});
+    this.dialog.closeAll();
+  }
+
+  close(){
+    this.dialog.closeAll();
+  }
+
+}
---
+++ spice/source/main/src/app/extra-pages/usertask/edit-item-modal/edit-item-modal.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Invoice } from 'src/app/models/invoice.model';
+import { Item } from 'src/app/models/item.model';
+
+
+@Component({
+  selector: 'app-edit-item-modal',
+  templateUrl: './edit-item-modal.component.html',
+  styleUrls: ['./edit-item-modal.component.scss']
+})
+export class EditItemModalComponent {
+
+  selectedItem: Item = new Item();
+  dialogConfig?: MatDialogConfig;
+   item: Item= new Item();
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private http: HttpClient, public dialog: MatDialog) {
+    this.selectedItem = data.item;
+     this.item= data.item;
+    // console.log("Selected Item is: ",this.selectedItem);
+  }
+
+
+  update() {
+    this.http.put<any>(`http://localhost:8000/api/users/${this.selectedItem.itemId}`, this.selectedItem).subscribe(data => {
+      console.log(this.selectedItem);
+    },
+      error => {      
+        console.error('An error occurred:', error);
+      }
+    );
+    this.dialog.closeAll();
+  }
+
+  close(){
+    this.dialog.closeAll();
+  }
+}
