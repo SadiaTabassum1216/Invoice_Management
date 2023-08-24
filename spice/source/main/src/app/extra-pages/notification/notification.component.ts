5742ae7d991a8cca01c11f669cf0bff74fb6c6f6
Sadia Tabassum <60074200+SadiaTabassum1216@users.noreply.github.com>
2023-08-24 23:32:38 +0600

    notification

--- spice/source/main/src/app/extra-pages/notification/notification.component.ts
+++ spice/source/main/src/app/extra-pages/notification/notification.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { httpOptions } from 'src/app/http_headers/httpOptions';
 import { backendEnvironment } from 'src/environments/backendEnvironment';
 
@@ -9,7 +10,7 @@ import { backendEnvironment } from 'src/environments/backendEnvironment';
   styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private router: Router){}
  
   ngOnInit(): void {
     this.getNotifications();
@@ -23,6 +24,16 @@ export class NotificationComponent implements OnInit {
       this.notificationList = data;
       console.log("Notification: ",this.notificationList);
     });
+  }
+
+  onNotificationClick(notification: any){
+    this.http.post<any>(`${backendEnvironment.apiUrl}/api/markAsRead/${notification.id}`, httpOptions)
+      .subscribe((data) => {
+        console.log(data);
+      });
+
+    this.router.navigate(['extra-pages','pricing_level']);
+   
   }
 
 
